c9b2bd0094ccbb569864028d2431ae7b36b60b36
stonemoore2 <stonemoore2@gmail.com>
2019-03-22 01:59:49 -0400

    created memory.cpp

---
+++ memory.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class memory{
+	vector<string> bits;
+public:
+	memory(int n);
+	string getword(int address);
+	void setword(int address, string new_word);
+};
+
+memory::memory(int n){
+	bits.resize(n);
+}
+
+string memory::getword(int address){
+	return bits[address];
+}
+
+void memory::setword(int address, string new_word){
+	bits[address] = new_word;
+}
