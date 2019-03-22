c073148f6a2c158db6d3a8f6b302e8b35aa874f5
stonemoore2 <stonemoore2@gmail.com>
2019-03-22 05:35:12 -0400

    filetostring for testing

--- encode.cpp
+++ encode.cpp
@@ -3,11 +3,7 @@
 #include <iostream>
 #include <vector>
 
-//#include <sstream>
-
 using namespace std;
-
-//string inpt = "int main(){\nint a[5]={16,2,77,40,5};\nint max=total(a);\n}";
 
 string char_to_binary(char c){
 	if (c == ' ')
@@ -23,25 +19,6 @@ string char_to_binary(char c){
 	return "error";
 }
 
-/*string char_to_binary(char c){
-	string c2(1, c);
-	ifstream infile("ascii_table.txt");
-	string line;
-	while(getline(infile, line)){
-		istringstream iss(line);
-		string inp, outp;
-		if (!(iss >> inp))
-			break;
-		if (inp == c2){
-			getline(infile, line);
-			istringstream iss2(line);
-			iss2 >> outp;
-			return outp;
-		}
-	}
-	return "error";
-}*/
-
 vector<string> string_to_binary(string input){
 	vector<string> result;
 	for (int i = 0; i < input.size(); i++){
@@ -55,7 +32,7 @@ vector<string> string_to_binary(string input){
 	return result;
 }
 
-int main(){
+//int main(){
 	/*char n;
 	cin.get(n);
 	cout << char_to_binary(n);*/
@@ -63,4 +40,4 @@ int main(){
 	/*vector<string> test = string_to_binary("I'm testing");
 	for (int i = 0; i < test.size(); i++)
 		cout << test[i] << " ";*/
-}
+//}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include "ram.cpp"
+#include "memory.cpp"
+#include "encode.cpp"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+string file_to_string(ifstream& in){
+    stringstream sstr;
+    sstr << in.rdbuf();
+    return sstr.str();
+}
+
+ifstream infile("input.txt");
+string foo = file_to_string(infile);
 
 int main(){
-
+	
 }
